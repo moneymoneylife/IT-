9a1ac635ca5d68c9dc2482530ba78a9ee6faa449
moneymoneylife <114158497+moneymoneylife@users.noreply.github.com>
2023-12-06 04:17:55 +0900

    찜 구현

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(session({
 }))
 
 // 정적 파일 제공을 위해 express.static 사용
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/downloads', express.static(path.join(__dirname, 'public/file')));
 app.use('/public/file', express.static(path.join(__dirname, 'public/file')));
-app.use('/img', express.static(path.join(__dirname, 'img')));
+
 app.use(bodyParser.json());
 
 let multer = require('multer');
@@ -58,22 +59,65 @@ app.get('/', function(req,res){
 });
 app.get('/zone',function(req,res){
     console.log("zone");
-    res.render('zone.ejs');
+    res.render('zone.ejs',{user:req.session.user});
 })
 
 app.get('/companies', function(req, res){
     console.log("companies");
-    res.render('companies.ejs');
+    mydb
+        .collection("list")
+        .find()
+        .toArray()
+        .then((result)=>{
+            res.render('companies.ejs', {data : result, user:req.session.user});
+            })
+});
+app.post('/sorting', (req, res) => {
+    const selectedValue = req.body.sorting;
+    if(selectedValue == '최근등록순'){
+        mydb
+        .collection("list")
+        .find()
+        .sort({currentDate:-1})
+        .toArray()
+        .then((result)=>{
+            res.render('companies.ejs', {data : result, user:req.session.user});
+            })
+    }else if(selectedValue == '인기순' ){
+
+    }else if(selectedValue == '인기순' ){
+        mydb
+        .collection("list")
+        .find()
+        .sort({id_:1})
+        .toArray()
+        .then((result)=>{
+            res.render('companies.ejs', {data : result, user:req.session.user});
+            })
+    }
+    
+});
+app.post('/delete', function(req, res){
+    console.log("delete");
+    console.log(req.body._id);
+    mydb.collection('like').deleteOne({like_id:req.body._id, userid:req.session.user.userid})
+    .then(result=>{
+        console.log("삭제완료");
+        res.status(200).send();
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).send();
+    })
+    
 });
 app.get('/contact', function(req, res){
     console.log("contact");
-    res.render('contact.ejs');
+    res.render('contact.ejs',{user:req.session.user});
 });
 app.get('/introduction', function(req, res){
     console.log("introduction");
-    res.render('introduction.ejs');
+    res.render('introduction.ejs',{user:req.session.user});
 });
-
 
 app.get("/login",function(req,res){
     res.render('login.ejs');
@@ -87,7 +131,7 @@ app.post("/login",function(req,res){
         .findOne({userid : req.body.userid, userpw : req.body.userpw})
         .then((result)=>{
             if(result== null){
-                res.send('로그인에 실패하셨습니다.');
+                res.render('login.ejs');
             }else{
                 req.session.user = result;
                 console.log('로그인');
@@ -97,63 +141,58 @@ app.post("/login",function(req,res){
         });
     
 }); 
-app.get("/signup",function(req,res){
-    res.render('signup.ejs',{data:'ㅇㅇㅇ'});
-});
-app.post("/signup",function(req,res){
-    console.log("signup");
-    mydb
-        .collection("user")
-        .findOne({userid : req.body.signup_userid})
-        .then((result)=>{
-            if(result == null){
-                console.log("아이디 : "+req.body.signup_userid);
-                console.log("비밀번호 : "+req.body.signup_userpw);
-                console.log("이름 : "+req.body.signup_username);
-                console.log("이메일 : "+req.body.signup_email);
-                console.log("전화번호 : "+req.body.signup_phone);
-                res.send('가입 되었습니다.');
-            
-                //몽고 DB 추가
-                mydb.collection('user').insertOne(
-                    {userid : req.body.signup_userid, 
-                    userpw : req.body.signup_userpw,
-                    username : req.body.signup_username,
-                    email : req.body.signup_email,
-                    phone : req.body.signup_phone
-                }
-                    ).then(result => {
-                        console.log(result);
-                        console.log('데이터 추가 성공');
-                    })
-            }else{
-                console.log('가입 실패');
-                res.render('signup.ejs', {data:result});
-            }
-        });
+app.get("/logout",function(req,res){
+    console.log("로그아웃");
+    req.session.destroy();
+    res.redirect('/');
 });
 
-app.post("/confirm_userid",function(req,res){
-    console.log("confirm_userid");
-    const confirm_userid = req.body.confirm_userid;
-    console.log(confirm_userid);
-    console.log(req.body.confirm_userid);
+app.get("/signup_user",function(req,res){
+    res.render('signup_user.ejs');
+});
+app.post("/signup_user",function(req,res){
+    console.log("signup_user");
+    const signup_userid = req.body.signup_userid;
+    const signup_userpw = req.body.signup_userpw;
+    const confirm_password = req.body.confirm_password;
+    const signup_username = req.body.signup_username;
+    const signup_email = req.body.signup_email;
+    const signup_phone = req.body.signup_phone;
+    console.log(signup_userpw,confirm_password);
 
-    mydb
-        .collection("user")
-        .findOne({userid : confirm_userid})
-        .then((result)=>{
-            if(result == null){
-                console.log('중복 없음');
-                res.status(200).send();
-            }else{
-                console.log('가입 실패');
-                res.status(500).send();
-            }
-        });
+    if(signup_userpw == confirm_password){
+        console.log("아이디 : "+signup_userid);
+        console.log("비밀번호 : "+signup_userpw);
+        console.log("이름 : "+signup_username);
+        console.log("이메일 : "+signup_email);
+        console.log("전화번호 : "+signup_phone);
+    
+        //몽고 DB 추가
+        mydb.collection('user').insertOne(
+            {
+            userid : signup_userid, 
+            userpw : signup_userpw,
+            username : signup_username,
+            email : signup_email,
+            phone : signup_phone,
+            type : "user"
+            })
+            .then(result => {
+                console.log(result);
+                console.log('데이터 추가 성공');
+            })
+            res.status(200).send();
+    }else{
+        console.log('가입 실패');
+        res.status(500).send();
+    }
 
 });
-app.post("/signup",function(req,res){
+
+app.get("/signup_company",function(req,res){
+    res.render('signup_company.ejs');
+});
+app.post("/signup_company",function(req,res){
     console.log("signup");
     const signup_userid = req.body.signup_userid;
     const signup_userpw = req.body.signup_userpw;
@@ -177,7 +216,8 @@ app.post("/signup",function(req,res){
             userpw : signup_userpw,
             username : signup_username,
             email : signup_email,
-            phone : signup_phone
+            phone : signup_phone,
+            type : "company"
             })
             .then(result => {
                 console.log(result);
@@ -190,13 +230,32 @@ app.post("/signup",function(req,res){
     }
 
 });
+app.post("/confirm_userid",function(req,res){
+    console.log("confirm_userid");
+    const confirm_userid = req.body.confirm_userid;
+    console.log(confirm_userid);
+    console.log(req.body.confirm_userid);
 
+    mydb
+        .collection("user")
+        .findOne({userid : confirm_userid})
+        .then((result)=>{
+            if(result == null){
+                console.log('중복 없음');
+                res.status(200).send();
+            }else{
+                console.log('가입 실패');
+                res.status(500).send();
+            }
+        });
+
+});
 app.get("/enter",function(req,res){
     console.log("enter");
     if(req.session.user){
-    res.render('enter.ejs');
+    res.render('enter.ejs',{user:req.session.user});
     }else{
-        res.send("로그인을 해주세요.");
+        res.redirect('/login');
     }
 
 });
@@ -211,13 +270,15 @@ app.post('/photo', upload.single('picture'),function(req,res){
 app.post('/save',function(req,res){
     // 현재 날짜 생성
     var currentDate = new Date();
-    console.log(req.body.someDate);
     //몽고 DB 추가
     mydb.collection('list').insertOne(
         {
             title : req.body.title, 
             content : req.body.content,
+            sal : req.body.sal, 
+            email : req.body.email,
             userid : req.session.user.userid,
+            local:req.body.local,
             path : imagepath,
             currentDate : currentDate
         }
@@ -226,14 +287,51 @@ app.post('/save',function(req,res){
             console.log('데이터 추가 성공');
             
         })
-        res.redirect('/list');
+        res.redirect('/companies');
         
 })
 
 
 app.get('/mypage',function(req,res){
     console.log("mypage");
-    res.render('mypage.ejs');
+    if(req.session.user){
+        res.render('mypage.ejs',{data:req.session.user});
+    }else{
+        res.redirect('/login');
+    }
+})
+
+app.get('/cart',function(req,res){
+    console.log("cart");
+    if(req.session.user){
+        mydb
+        .collection("like")
+        .find({userid : req.session.user.userid})
+        .toArray()
+        .then((like_result)=>{
+            if (like_result.length > 0) { // 배열에 결과가 있는지 확인
+                console.log(like_result[0].like_id); // 첫 번째 객체의 like_id 출력
+                const objectIdArray = like_result.map(item => new ObjectId(item.like_id));
+                console.log(objectIdArray); // ObjectId 배열 출력
+
+                mydb
+                    .collection("list")
+                    .find({ _id: { $in: objectIdArray } })
+                    .toArray()
+                    .then((list_result) => {
+                        console.log(list_result);
+                        res.render('cart.ejs', { data: list_result, user: req.session.user });
+                    })
+                    .catch(err => {
+                        console.error("Error finding documents: ", err);
+                        res.redirect('/error'); // 에러 페이지로 리다이렉트
+                    });
+            } else {
+
+            }})
+    }else{
+        res.redirect('/login');
+    }
 })
 app.post('/file', upload.single('file'),function(req,res){
     console.log(req.file.path);
@@ -264,7 +362,7 @@ app.get("/list", function(req, res){
         .find()
         .toArray()
         .then((result)=>{
-                res.render('list.ejs', {data : result});
+                res.render('list.ejs', {data : result, user:req.session.user});
             })
 });
 
@@ -278,6 +376,50 @@ app.get("/content/:id", (req, res) => {
         console.log(result);
         res.render('content.ejs', {data : result});
         })
+});
+app.get("/details/:id", (req, res) => {
+    console.log(req.params.id);
+    req.params.id = new ObjectId(req.params.id);
+    mydb
+    .collection("list")
+    .findOne({_id : req.params.id})
+    .then((result)=>{
+        console.log(result);
+        res.render('details.ejs', {data : result, user:req.session.user});
+        })
+});
+
+app.post("/details/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log("/details/:id");
+    const newid = req.params.id.substring(1); // 첫 번째 글자 제거
+    console.log(newid); 
+    if(req.session.user){
+        mydb
+        .collection("like")
+        .findOne({like_id : newid, userid : req.session.user.userid})
+        .then((result)=>{
+            if(result==null){
+                //몽고 DB 추가
+            mydb.collection('like').insertOne(
+                {
+                    like_id : newid,
+                    userid : req.session.user.userid
+                })
+                .then(result => {
+                    console.log(result);
+                    console.log('데이터 추가 성공');
+                    res.redirect('/cart');
+                })
+            }else{
+                console.log('데이터 추가 실패');
+                res.status(500).send();
+            }
+        })
+    }else{
+        res.redirect('/login');
+        console.log('찜 실패');
+    }
 });
 app.get('/test', function(req, res){
     mydb
@@ -298,8 +440,8 @@ app.get('/test', function(req, res){
 
 
 //css
-app.get('/sty.css', function(req, res){
-    res.sendFile(__dirname + '/css/sty.css');
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/css/style.css');
 });
 app.get('/zone.css', function(req, res){
     res.sendFile(__dirname + '/css/zone.css');
@@ -327,4 +469,10 @@ app.get('/list.css', function(req, res){
 });
 app.get('/signup.css', function(req, res){
     res.sendFile(__dirname + '/css/signup.css');
+});
+app.get('/details.css', function(req, res){
+    res.sendFile(__dirname + '/css/details.css');
+});
+app.get('/cart.css', function(req, res){
+    res.sendFile(__dirname + '/css/cart.css');
 });
